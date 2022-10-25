59939cc0688e6232996da70c0aa0a39f622d6522
augustasjagela <106032669+augustasjagela@users.noreply.github.com>
2022-10-25 19:11:46 +0300

    3

---
+++ form.php
@@ -0,0 +1,19 @@
+<div class="formDiv">
+        <form class="form" action="index.html">
+            <label for="product">Choose product</label>
+            <select id="product" name="product">
+                <option value="businesscards">Business Cards</option>
+                <option value="flyers">Flyers</option>
+                <option value="posters">Posters</option>
+            </select>
+            <label for="paper">Choose paper</label>
+            <select id="paper" name="paper">
+                <option value="premium">Premium paper</option>
+                <option value="basic">Basic paper</option>
+                <option value="deco">Decorative paper</option>
+            </select>
+            <label for="qty">Quantity</label>
+            <input type="number" name="qty" id="qty">
+            <input class="submitButton" type="submit">
+        </form>
+    </div>
